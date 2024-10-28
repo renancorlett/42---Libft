c0ba791e25af1a8a9285f94eefb099ca69fd9745
Renan Corlett <renancorlett@gmail.com>
2024-10-28 09:50:56 +0000

    Add files via upload

--- ft_split.c
+++ ft_split.c
@@ -5,115 +5,100 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: rcorlett <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/10/21 15:09:42 by rcorlett          #+#    #+#             */
-/*   Updated: 2024/10/28 08:36:04 by rcorlett         ###   ########.fr       */
+/*   Created: 2024/10/28 09:38:59 by rcorlett          #+#    #+#             */
+/*   Updated: 2024/10/28 09:47:03 by rcorlett         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-//function = Allocates with malloc and returns an array of string obtained by
-//splitting 's' using a char 'c' as a delimiter. Must end with a NULL pointer;
-// s	= String to be splitted;
-// c	= Delimiter character;
-//if returns array of a new string from the split or NULL in the allocation=FAIL
-
 #include "libft.h"
 
-static int	ft_wordcount(char const *s, char c)
+static int	count_words(char const *s, char c)
 {
-	int	count;
 	int	i;
+	int	words;
 
-	count = 0;
 	i = 0;
+	words = 0;
 	while (s[i])
 	{
-		while (s[i] == c)
-			i++;
-		if (s[i] != '\0')
+		if (s[i] != c)
 		{
-			count++;
+			words++;
 			while (s[i] != c && s[i])
 				i++;
 		}
+		else
+			i++;
 	}
-	return (count);
+	return (words);
 }
 
-static int	ft_wordlen(char const *s, char c, int start)
+static int	get_substring_length(char const *s, char c, int i)
 {
-	int	i;
-	int	count;
+	int	len;
 
-	i = start;
-	count = 0;
-	while (s[i] && s[i] != c)
+	len = 0;
+	while (s[i] != c && s[i])
 	{
-		count++;
 		i++;
+		len++;
 	}
-	return (count);
+	return (len);
 }
 
-static char	*ft_getword(char const *s, char c, int start)
+static void	free_memory(char **arr, int j)
 {
-	char	*word;
-	int		len;
-	int		i;
-	int		j;
+	while (--j >= 0)
+		free(arr[j]);
+	free(arr);
+}
 
-	i = start;
-	j = 0;
-	len = ft_wordlen(s, c, start);
-	word = (char *)malloc(sizeof(char) * (len + 1));
-	if (!word)
-		return (NULL);
-	while (s[i] && s[i] != c)
-		word[j++] = s[i++];
-	word[j] = '\0';
-	return (word);
+static char	*extract_substring(char const *s, int i, char c)
+{
+	char	*substring;
+	int		substring_len;
+
+	substring_len = get_substring_length(s, c, i);
+	substring = ft_substr(s, i, substring_len);
+	return (substring);
 }
 
 char	**ft_split(char const *s, char c)
 {
+	int		words;
 	int		i;
 	int		j;
 	char	**arr;
 
-	i = 0;
-	j = -1;
 	if (!s)
 		return (NULL);
-	arr = (char **)malloc(sizeof(char *) * (ft_wordcount(s, c) + 1));
+	words = count_words(s, c);
+	arr = (char **)malloc((words + 1) * sizeof(char *));
 	if (!arr)
 		return (NULL);
-	while (s[i])
+	i = 0;
+	j = 0;
+	while (j < words)
 	{
 		while (s[i] == c)
 			i++;
-		if (s[i] != '\0')
-			arr[++j] = ft_getword(s, c, i);
-		while (s[i] && s[i] != c)
-			i++;
+		arr[j] = extract_substring(s, i, c);
+		if (!arr[j])
+			return (free_memory(arr, j), NULL);
+		i += get_substring_length(s, c, i);
+		j++;
 	}
-	arr[++j] = NULL;
+	arr[j] = NULL;
 	return (arr);
 }
 
 /*#include <stdio.h>
 int main(void)
 {
-	char **result = ft_split(Renan=Corlett", '=');
-	int	i;
-	
-	if (result)
-	{
-		while (result[i])
-		{
-			printf("Word %d: %s\n", i, result[i]);
-			free(result[i]);
-			i++;
-		}
-		free (result);
-	}
-	return (0);
+    char **result;
+    result = ft_split("Renan Corlett Silva", ' ');
+    printf("Word 1: %s\n", result[0]);
+    printf("Word 2: %s\n", result[1]);
+    printf("Word 3: %s\n", result[2]);
+    return (0);
 }*/
